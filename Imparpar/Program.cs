e7fae4d03a13fe63afe6239cc17253c13f6adfe0
Vinicius Fernandes <vinicius.fernandes@bunzlbrasil.protcapad.com.br>
2020-06-02 17:53:05 -0300

    Novo

---
+++ Imparpar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imparpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+           
+            Console.Write("Digite o numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("O numero é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero é impar");
+            }
+
+            Console.ReadLine();
+        }
+        }
+    }
+
